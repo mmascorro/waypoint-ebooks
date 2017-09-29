67569cabf792658e0d448eb5246a7ac84f0e4e06
Miguel Mascorro <futuretuesday@gmail.com>
2017-09-28 22:49:20 -0500

    fix & cleanup

--- grabarticles.py
+++ grabarticles.py
@@ -4,41 +4,41 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 
 
-def scrape_waypoint:
-	pages = 179
-	current_page = 1;
-	article_url_base = 'https://waypoint.vice.com/api/v1/latest?locale=en_us&page={}&per_page=12'
+def scrape_waypoint():
+    pages = 181
+    current_page = 1
+    article_url_base = 'https://waypoint.vice.com/api/v1/latest?locale=en_us&page={}&per_page=12'
 
-	headers = {
-	    'User-Agent': 'waypoint_ebooks 1.0'
-	}
+    headers = {
+        'User-Agent': 'waypoint_ebooks 1.0'
+    }
 
-	f = open('corpus.txt', 'w+')
+    f = open('./data/corpus.txt', 'w+')
 
-	while current_page <= pages:
+    while current_page <= pages:
 
-	    r = requests.get(article_url_base.format(current_page), headers=headers)
+        r = requests.get(article_url_base.format(current_page), headers=headers)
 
-	    for article in r.json():
-		print(article['data']['title'])
+        for article in r.json():
+            print(article['data']['title'])
 
-		f.write(article['data']['title'])
-		f.write("\n")
-
-
-		soup = BeautifulSoup(article['data']['body'], 'html5lib')
-		article_body = soup.get_text()
-
-		f.write(article_body)
-		f.write("\n\n\n")
+            f.write(article['data']['title'])
+            f.write("\n")
 
 
-	    current_page += 1
+            soup = BeautifulSoup(article['data']['body'], 'html5lib')
+            article_body = soup.get_text()
 
-	    time.sleep(6)
+            f.write(article_body)
+            f.write("\n\n\n")
 
-	f.close()
+
+            current_page += 1
+
+            time.sleep(5)
+
+    f.close()
 
 
 if __name__ == "__main__":
-	scrape_waypoint()
+    scrape_waypoint()
