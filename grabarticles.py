b44ff10f53e7028ff16068eebbee45291d22a548
Miguel Mascorro <futuretuesday@gmail.com>
2017-09-29 00:50:23 -0500

    grab total article count first

--- grabarticles.py
+++ grabarticles.py
@@ -1,23 +1,29 @@
 import requests
 import time
+import math
 from bs4 import BeautifulSoup
 from pprint import pprint
 
-
 def scrape_waypoint():
-    pages = 181
     current_page = 1
-    article_url_base = 'https://waypoint.vice.com/api/v1/latest?locale=en_us&page={}&per_page=12'
+    articles_per_page = 12
+    corpusFile = './data/corpus.txt'
+
+    article_url_base = 'https://waypoint.vice.com/api/v1/latest?locale=en_us&page={}&per_page={}'
 
     headers = {
         'User-Agent': 'waypoint_ebooks 1.0'
     }
+    #determine total number of pages from api headers
+    total_request = requests.get(article_url_base.format(current_page,articles_per_page), headers=headers)
+    total_articles = int(total_request.headers['x-total-count'])
+    total_pages = math.ceil(total_articles/articles_per_page)
 
-    f = open('./data/corpus.txt', 'w+')
+    f = open(corpusFile, 'w+')
 
-    while current_page <= pages:
+    while current_page <= total_pages:
 
-        r = requests.get(article_url_base.format(current_page), headers=headers)
+        r = requests.get(article_url_base.format(current_page,articles_per_page), headers=headers)
 
         for article in r.json():
             print(article['data']['title'])
@@ -25,20 +31,17 @@ def scrape_waypoint():
             f.write(article['data']['title'])
             f.write("\n")
 
-
             soup = BeautifulSoup(article['data']['body'], 'html5lib')
             article_body = soup.get_text()
 
             f.write(article_body)
             f.write("\n\n\n")
 
-
             current_page += 1
 
         time.sleep(5)
 
     f.close()
-
 
 if __name__ == "__main__":
     scrape_waypoint()
